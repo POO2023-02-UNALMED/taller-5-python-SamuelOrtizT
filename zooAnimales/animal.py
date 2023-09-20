a23d8cd19ab742139666d4a246f8f6fac04fcaac
SamuelOrtizT <saortizt@unal.edu.co>
2023-09-20 15:07:48 -0500

    Update animal.py

--- zooAnimales/animal.py
+++ zooAnimales/animal.py
@@ -16,7 +16,7 @@ class Animal ():
         from zooAnimales.mamifero import Mamifero
         from zooAnimales.pez import Pez
         from zooAnimales.reptil import Reptil
-        return "Mamiferos :" + str(Mamifero.cantidadMamiferos()) + "\nAves :" + str(Ave.cantidadAves()) + "\nReptiles :" + str(Reptil.cantidadReptiles()) + "\nPeces :" + str(Pez.cantidadPeces()) + "\nAnfibios :" + str(Anfibio.cantidadAnfibios())
+        return "Mamiferos : " + str(Mamifero.cantidadMamiferos()) + "\nAves : " + str(Ave.cantidadAves()) + "\nReptiles : " + str(Reptil.cantidadReptiles()) + "\nPeces : " + str(Pez.cantidadPeces()) + "\nAnfibios : " + str(Anfibio.cantidadAnfibios())
     def toString (self):
         from gestion.zoologico import Zoologico
         if self._zona == None:
