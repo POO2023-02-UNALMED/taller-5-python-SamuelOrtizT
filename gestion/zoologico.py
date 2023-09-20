ea86c3339eaf15ca960d8efe380e9c501df01296
SamuelOrtizT <saortizt@unal.edu.co>
2023-09-20 13:51:23 -0500

    b

---
+++ gestion/zoologico.py
@@ -0,0 +1,24 @@
+class Zoologico():
+    def __init__(self, nombre, ubicacion):
+        self._nombre = nombre
+        self._ubicacion = ubicacion
+        self._zonas = []
+    def agregarZonas(self, zona1):
+        self._zonas.append(zona1)
+    def cantidadTotalAnimales (self):
+        a = 0
+        for zona in self._zonas:
+            a += zona.cantidadAnimales()
+        return a
+    def getNombre (self):
+        return self._nombre
+    def setNombre (self, nombre):
+        self._nombre = nombre
+    def getUbicacion (self):
+        return self._ubicacion
+    def setUbicacion (self, ubicacion):
+        self._ubicacion = ubicacion
+    def getZona (self):
+        return self._zonas
+    def setZona (self, zonas):
+        self._zonas = zonas
---
+++ zooAnimales/pez.py
@@ -0,0 +1,49 @@
+from zooAnimales.animal import Animal
+class Pez (Animal):
+    listado = []
+    salmones = 0
+    bacalaos = 0
+    def __init__ (self, nombre, edad, habitat, genero, colorEscamas, cantidadAletas):
+        super().__init__(nombre, edad, habitat, genero)
+        self._colorEscamas = colorEscamas
+        self._cantidadAletas = cantidadAletas
+        Pez.listado.append(self)
+    @classmethod
+    def cantidadPeces (cls):
+        return len(cls.listado)
+    def movimiento (self):
+        return "nadar"
+    @classmethod 
+    def crearSalmon (cls, nombre, edad, genero):
+        cls.salmones += 1
+        return Pez(nombre, edad, "oceano", genero, "rojo", 6)
+    @classmethod 
+    def crearBacalao (cls, nombre, edad, genero):
+        cls.bacalaos += 1
+        return Pez(nombre, edad, "oceano", genero, "blanco", 6)
+    @classmethod 
+    def getListado (cls):
+        return cls.listado
+    @classmethod 
+    def setListado (cls, listado):
+        cls.listado = listado
+    @classmethod 
+    def getSalmones (cls):
+        return cls.salmones
+    @classmethod 
+    def setSalmones (cls, salmones):
+        cls.salmones = salmones
+    @classmethod 
+    def getBacalaos (cls):
+        return cls.bacalaos
+    @classmethod 
+    def setBacalaos (cls, bacalaos):
+        cls.bacalaos = bacalaos
+    def getColorEscamas (self):
+        return self._colorEscamas
+    def setColorEscamas (self, colorEscamas):
+        self._colorEscamas = colorEscamas
+    def getcantidadAletas (self):
+        return self._cantidadAletas
+    def setcantidadAletas (self, cantidadAletas):
+        self._cantidadAletas = cantidadAletas
